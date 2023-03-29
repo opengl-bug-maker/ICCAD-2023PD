8b03a1bb2f78db2b81120c61ea18493b53155d33
LenfIcefrost <david77988@gmail.com>
2023-03-29 19:28:36 +0800

    feat: complete vec2d from position
    half xy, length, 2d area, operator

---
+++ src/utilities/vec2d_t.cpp
@@ -0,0 +1,61 @@
+//
+// Created by 林士傑 on 2023/3/9.
+//
+
+#include "vec2d_t.h"
+
+vec2d_t vec2d_t::default_position = vec2d_t(-1, -1);
+
+vec2d_t::vec2d_t() : x(default_position.get_x()), y(default_position.get_y()) {
+
+}
+
+vec2d_t::vec2d_t(const double& x, const double& y) : x(x), y(y) {}
+
+double vec2d_t::get_x() const {
+    return x;
+}
+
+double vec2d_t::get_y() const {
+    return y;
+}
+
+double vec2d_t::get_half_x() const {
+    return get_x() / 2.0;
+}
+
+double vec2d_t::get_half_y() const {
+    return get_y() / 2.0;
+}
+
+double vec2d_t::get_length() const {
+    return sqrt(get_x() * get_x() + get_y() * get_y());
+}
+
+double vec2d_t::get_area() const {
+    return get_x() * get_y();
+}
+
+vec2d_t vec2d_t::operator+(const vec2d_t &vec2D) const {
+    return {this->get_x() + vec2D.get_x(), this->get_y() + vec2D.get_y()};
+}
+
+vec2d_t vec2d_t::operator-(const vec2d_t &vec2D) const {
+    return {this->get_x() - vec2D.get_x(), this->get_y() - vec2D.get_y()};
+}
+
+vec2d_t vec2d_t::operator*(const double& mul) const {
+    return {this->get_x() * mul, this->get_y() * mul};
+}
+
+vec2d_t vec2d_t::operator/(const double& div) const {
+    return {this->get_x() / div, this->get_y() / div};
+}
+
+bool vec2d_t::operator==(const vec2d_t &vec2D) const {
+    return fabs(this->get_x() - vec2D.get_x()) <= 1E-6 && fabs(this->get_y() - vec2D.get_y()) <= 1E-6;
+}
+
+bool vec2d_t::operator!=(const vec2d_t &vec2D) const {
+    return fabs(this->get_x() - vec2D.get_x()) > 1E-6 || fabs(this->get_y() - vec2D.get_y()) > 1E-6;
+}
---
+++ src/utilities/vec2d_t.h
@@ -0,0 +1,46 @@
+
+//
+// Created by 林士傑 on 2023/3/9.
+//
+
+#ifndef INC_2023PD_VEC2D_T_H
+#define INC_2023PD_VEC2D_T_H
+
+#include <cmath>
+
+class vec2d_t {
+    double x, y;
+public:
+    static vec2d_t default_position;
+
+    vec2d_t();
+
+    vec2d_t(const double& x, const double& y);
+
+    double get_x() const;
+
+    double get_y() const;
+
+    double get_half_x() const;
+
+    double get_half_y() const;
+
+    double get_length() const;
+
+    double get_area() const;
+
+    vec2d_t operator+(const vec2d_t& vec2D) const;
+
+    vec2d_t operator-(const vec2d_t& vec2D) const;
+
+    vec2d_t operator*(const double& mul) const;
+
+    vec2d_t operator/(const double& div) const;
+
+    bool operator==(const vec2d_t& vec2D) const;
+
+    bool operator!=(const vec2d_t& vec2D) const;
+};
+
+
+#endif //INC_2023PD_VEC2D_T_H
