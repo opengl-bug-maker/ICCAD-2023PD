267138e1c330e41bb5411a0e09e9ffd615951d79
LenfIcefrost <david77988@gmail.com>
2023-03-29 20:16:37 +0800

    feat: complete static chip
    make it static

--- src/static_data/chip_t.cpp
+++ src/static_data/chip_t.cpp
@@ -6,6 +6,15 @@
 #include "soft_module_t.h"
 #include "fixed_module_t.h"
 
+int chip_t::width, chip_t::height;
+int chip_t::softCount, chip_t::fixedCount;
+std::unordered_map<std::string, int> chip_t::moduleNameToIndex;
+
+std::vector<module_t*> chip_t::modules;
+std::vector<soft_module_t*> chip_t::soft_modules;
+std::vector<fixed_module_t*> chip_t::fixed_modules;
+std::vector<std::vector<int>> chip_t::connectionTable;
+
 void chip_t::file_input(std::string fileName) {
     std::fstream file;
     file.open(fileName);
@@ -15,40 +24,64 @@ void chip_t::file_input(std::string fileName) {
 
     while(file >> temp){
         if(temp == "CHIP"){
-            file >> this->width >> this->height;
+            file >> chip_t::width >> chip_t::height;
         }else if(temp == "SOFTMODULE"){
-            file >> softCount;
-            modules.resize(softCount);
-            for (int i = 0; i < softCount; ++i) {
-                modules[i] = soft_module_t::fileInput(file);
-                moduleNameToIndex[modules[i]->getName()] = i;
+            file >> chip_t::softCount;
+            chip_t::modules.resize(chip_t::softCount);
+            chip_t::soft_modules.resize(chip_t::softCount);
+            for (int i = 0; i < chip_t::softCount; ++i) {
+                chip_t::soft_modules[i] = soft_module_t::fileInput(file);
+                chip_t::modules[i] = chip_t::soft_modules[i];
+                chip_t::moduleNameToIndex[chip_t::modules[i]->getName()] = i;
             }
         }else if(temp == "FIXEDMODULE"){
-            file >> fixedCount;
-            modules.resize(softCount + fixedCount);
-            for (int i = softCount; i < softCount + fixedCount; ++i) {
-                modules[i] = fixed_module_t::fileInput(file);
-                moduleNameToIndex[modules[i]->getName()] = i;
+            file >> chip_t::fixedCount;
+            chip_t::modules.resize(chip_t::softCount + chip_t::fixedCount);
+            chip_t::fixed_modules.resize(chip_t::fixedCount);
+            for (int i = chip_t::softCount; i < chip_t::softCount + chip_t::fixedCount; ++i) {
+                chip_t::fixed_modules[i - chip_t::softCount] = fixed_module_t::fileInput(file);
+                chip_t::modules[i] = chip_t::fixed_modules[i - chip_t::softCount];
+                chip_t::moduleNameToIndex[chip_t::modules[i]->getName()] = i;
             }
         }else if(temp == "CONNECTION"){
             int connectionCount, index0, index1;
             file >> connectionCount;
-            connectionTable = std::vector<std::vector<int>>(this->softCount + this->fixedCount, std::vector<int>(this->softCount + this->fixedCount, 0));
+            chip_t::connectionTable =
+                    std::vector<std::vector<int>>(chip_t::softCount + chip_t::fixedCount,
+                    std::vector<int>(chip_t::softCount + chip_t::fixedCount, 0));
             for (int i = 0; i < connectionCount; ++i) {
                 file >> temp;
-                index0 = moduleNameToIndex[temp];
+                index0 = chip_t::moduleNameToIndex[temp];
                 file >> temp;
-                index1 = moduleNameToIndex[temp];
+                index1 = chip_t::moduleNameToIndex[temp];
                 file >> iTemp;
 
-                connectionTable[index0][index1] = iTemp;
-                modules[index0]->getConnections().emplace_back(modules[index1], iTemp);
-                modules[index1]->getConnections().emplace_back(modules[index0], iTemp);
+                chip_t::connectionTable[index0][index1] = iTemp;
+                chip_t::modules[index0]->connections.emplace_back(chip_t::modules[index1], iTemp);
+                chip_t::modules[index1]->connections.emplace_back(chip_t::modules[index0], iTemp);
             }
         }else {
             throw std::exception();
         }
     }
+}
 
-    int i = 0;
+const int chip_t::getWidth() {
+    return chip_t::width;
+}
+
+const int chip_t::getHeight() {
+    return chip_t::height;
+}
+
+const std::vector<module_t *> &chip_t::getModules() {
+    return chip_t::modules;
+}
+
+const std::vector<soft_module_t *> &chip_t::getSoftModules() {
+    return chip_t::soft_modules;
+}
+
+const std::vector<fixed_module_t *> &chip_t::getFixedModules() {
+    return chip_t::fixed_modules;
 }
--- src/static_data/chip_t.h
+++ src/static_data/chip_t.h
@@ -10,19 +10,36 @@
 #include <fstream>
 #include <exception>
 #include <unordered_map>
-#include "module_t.h"
+//#include "static_data/module_t.h"
+//#include "static_data/soft_module_t.h"
+//#include "static_data/fixed_module_t.h"
+
+class module_t;
+class soft_module_t;
+class fixed_module_t;
 
 class chip_t {
-public:
-    int width, height;
-    int softCount, fixedCount;
-    std::unordered_map<std::string, int> moduleNameToIndex;
+    static int width, height;
+    static int softCount, fixedCount;
+    static std::unordered_map<std::string, int> moduleNameToIndex;
 
-    std::vector<module_t*> modules;
-    std::vector<std::vector<int>> connectionTable;
+    static std::vector<module_t*> modules;
+    static std::vector<soft_module_t*> soft_modules;
+    static std::vector<fixed_module_t*> fixed_modules;
+    static std::vector<std::vector<int>> connectionTable;
 
 public:
-    void file_input(std::string fileName);
+    static void file_input(std::string fileName);
+
+    static const int getWidth();
+
+    static const int getHeight();
+
+    static const std::vector<module_t *> &getModules();
+
+    static const std::vector<soft_module_t *> &getSoftModules();
+
+    static const std::vector<fixed_module_t *> &getFixedModules();
 };
 
 
