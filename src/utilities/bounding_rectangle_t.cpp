d926eb8c4f10f6fc2a0d092dd13b48e34c92d717
LenfIcefrost <david77988@gmail.com>
2023-03-29 20:00:17 +0800

    feat: complete bounding rectangle
    from 0

--- src/utilities/bounding_rectangle_t.cpp
+++ src/utilities/bounding_rectangle_t.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "bounding_rectangle_t.h"
+
+bounding_rectangle_t::bounding_rectangle_t(module_t *link_module) : link_module(link_module), rect(vec2d_t(0, 0), vec2d_t(0, 0)){
+
+}
+
+bounding_rectangle_t::bounding_rectangle_t(module_t *link_module, rect_t rect) : link_module(link_module), rect(rect){
+
+}
+
+
+const rect_t &bounding_rectangle_t::getRect() const {
+    return rect;
+}
+
+const module_t * const bounding_rectangle_t::getLinkModule() const {
+    return link_module;
+}
--- src/utilities/bounding_rectangle_t.h
+++ src/utilities/bounding_rectangle_t.h
@@ -5,9 +5,26 @@
 #ifndef INC_2023PD_BOUNDING_RECTANGLE_T_H
 #define INC_2023PD_BOUNDING_RECTANGLE_T_H
 
+#include "utilities/rect_t.h"
+
+class module_t;
 
 class bounding_rectangle_t {
+    friend class module_t;
+    friend class fixed_module_t;
+    friend class soft_module_t;
+    module_t* link_module;
+    rect_t rect;
 
+    bounding_rectangle_t(module_t* link_module);
+
+    bounding_rectangle_t(module_t* link_module, rect_t rect);
+public:
+    bounding_rectangle_t() = delete;
+
+    const rect_t &getRect() const;
+
+    const module_t * const getLinkModule() const;
 };
 
 
