66d4496af49aa9cf918e3282b403c256ca2e93f8
LenfIcefrost <david77988@gmail.com>
2023-04-08 00:56:55 +0800

    feat: quadratic random position
    quadratic version 0

--- src/quadratic/quadratic_t.cpp
+++ src/quadratic/quadratic_t.cpp
@@ -2,6 +2,7 @@
 // Created by 林士傑 on 2023/3/27.
 //
 
+#include <random>
 #include "quadratic_t.h"
 std::vector<vec2d_t> quadratic_t::final_position;
 std::vector<vec2d_t> quadratic_t::slope;
@@ -12,7 +13,11 @@ vec2d_t quadratic_t::get_coor(int index) {
 }
 
 vec2d_t quadratic_t::get_coor_random(int index) {
-    return vec2d_t();
+    std::default_random_engine rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> xDis(0, chip_t::get_width());
+    std::uniform_int_distribution<> yDis(0, chip_t::get_height());
+    return vec2d_t(xDis(gen), yDis(gen));
 }
 
 void quadratic_t::set_floopplanning(const std::vector<bounding_rectangle_t> &bounding_rectangles,
