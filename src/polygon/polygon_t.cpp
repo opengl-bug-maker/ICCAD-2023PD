f67e525c1519b048e73c6eef4af9ce29305142b0
LenfIcefrost <david77988@gmail.com>
2023-04-20 14:00:25 +0800

    fix: polygon bounding rect collision

--- src/polygon/polygon_t.cpp
+++ src/polygon/polygon_t.cpp
@@ -14,12 +14,12 @@ std::vector<bounding_rectangle_t> &polygon_t::get_rects() {
 }
 
 bool polygon_t::is_bounding_collision(const bounding_rectangle_t &rect) const {
-    return !(
-            rect.getRect().get_center().get_x() >= this->bounding_rect.get_center().get_x() + this->bounding_rect.get_size().get_x() ||
-            rect.getRect().get_center().get_y() + rect.getRect().get_size().get_y() <= this->bounding_rect.get_center().get_y() ||
-            rect.getRect().get_center().get_x() + rect.getRect().get_size().get_x() <= this->bounding_rect.get_center().get_x() ||
-            rect.getRect().get_center().get_y() >= this->bounding_rect.get_center().get_y() + this->bounding_rect.get_size().get_y()
-            );
+    return (
+            rect.getRect().get_center().get_x() - rect.getRect().get_size().get_half_x() < this->bounding_rect.get_center().get_x() + this->bounding_rect.get_size().get_half_x() &&
+            rect.getRect().get_center().get_y() + rect.getRect().get_size().get_half_y() > this->bounding_rect.get_center().get_y() - this->bounding_rect.get_size().get_half_y() &&
+            rect.getRect().get_center().get_x() + rect.getRect().get_size().get_half_x() > this->bounding_rect.get_center().get_x() - this->bounding_rect.get_size().get_half_x() &&
+            rect.getRect().get_center().get_y() - rect.getRect().get_size().get_half_y() < this->bounding_rect.get_center().get_y() + this->bounding_rect.get_size().get_half_y()
+    );
 }
 
 bool polygon_t::is_collision(bounding_rectangle_t &rect) {
