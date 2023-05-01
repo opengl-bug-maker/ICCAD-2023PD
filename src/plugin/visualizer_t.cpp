0a3d9eab14066e2c40119d22145be155156bc501
lkj0096 <lkj9487@gmail.com>
2023-05-02 01:16:33 +0800

    refactor: change show_fp function type

--- src/plugin/visualizer_t.cpp
+++ src/plugin/visualizer_t.cpp
@@ -15,8 +15,16 @@ void my_out(const std::string& str){
     std::cout << str << std::endl;
 #endif
 }
-void visualizer_t::show_fp(std::vector<bounding_rectangle_t> & bd_rect) {
     srand(time(NULL));
+void visualizer_t::show_fp(const std::vector<bounding_rectangle_t>& bd_rect) {
+    std::vector<std::pair<std::vector<vec2d_t>,std::string>> bd_rect_pt;
+    for(auto & i : bd_rect){
+        const double rx = i.getRect().get_right_upper().get_x();
+        const double lx = i.getRect().get_left_lower().get_x();
+        const double uy = i.getRect().get_right_upper().get_y();
+        const double ly = i.getRect().get_left_lower().get_y();
+        bd_rect_pt.push_back({std::vector<vec2d_t>{{lx, ly}, {rx, ly}, {rx, uy}, {lx,uy}},
+                              i.getLinkModule()->getName()});
     }
     return ;
 }
