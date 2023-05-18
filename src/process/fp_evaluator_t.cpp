81b2afa9a403f557e0d4f8ddab8d5dc94a81e047
lkj0096 <lkj9487@gmail.com>
2023-05-18 10:18:58 +0800

    feat: let evaluator argument use reference

--- src/process/fp_evaluator_t.cpp
+++ src/process/fp_evaluator_t.cpp
@@ -4,7 +4,7 @@
 #include "fp_evaluator_t.h"
 using std::cout;
 using std::endl;
-uint32_t fp_evaluator_t::get_score(floorplanning_t  fp) {
+uint32_t fp_evaluator_t::get_score(floorplanning_t& fp) {
     size_t deg_c = 0;
     int wh = chip_t::get_width()+chip_t::get_height();
     for(size_t i = 0; i<fp.get_soft_rect_n(); ++i){
@@ -18,7 +18,7 @@ uint32_t fp_evaluator_t::get_score(floorplanning_t  fp) {
     return wirelength_cost*0.8+0.2*dead_space_cost;
 }
 
-uint32_t fp_evaluator_t::get_dead_space(floorplanning_t fp) {
+uint32_t fp_evaluator_t::get_dead_space(floorplanning_t& fp) {
     vector<rect_t> empty_rects = fp.get_polygon_forest().get_empty_spaces();
     uint32_t dead_area = 0;
     for(auto& empty_rect:empty_rects){
