f626349709b732847180ab863b4287ac72918452
lkj0096 <lkj9487@gmail.com>
2023-06-15 11:52:23 +0800

    feat: change window name

--- src/plugin/visualizer_t.h
+++ src/plugin/visualizer_t.h
@@ -6,16 +6,19 @@
 #define INC_2023PD_VISUALIZER_T_H
 
 #include <vector>
+#include <string>
 #include "utilities/bounding_rectangle_t.h"
 
 class visualizer_t {
 public:
     static void show_fp(const std::vector<bounding_rectangle_t>&);
-    static void show_fp_no_border(const std::vector<bounding_rectangle_t>&);
-    static void show_fp_rect_no_border(const std::vector<std::pair<rect_t, std::string>> &);
+    static void show_fp_no_border(const std::vector<bounding_rectangle_t>&, const std::string&);
+    static void show_fp_rect_no_border(const std::vector<std::pair<rect_t, std::string>> &, const std::string&);
+    static void set_window_name(const std::string&);
 private:
     static int max_x;
     static int max_y;
+    static std::string name;
     static void join(const std::vector<std::pair<std::vector<vec2d_t>,std::string>>& polys_pts);
     static void gnup_th(const std::vector<std::pair<std::vector<vec2d_t>,std::string>> polys_pts);
 };
