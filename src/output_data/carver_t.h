a512bf7ac496c09143a6496b45e3aeaa38cb1a8e
IceFrostLenf <david77988@gmail.com>
2023-09-02 19:33:52 +0800

    fix: force check best wirelength

--- src/output_data/carver_t.h
+++ src/output_data/carver_t.h
@@ -2,12 +2,12 @@
 // Created by 林士傑 on 2023/8/28.
 //
 
-#ifndef ICCAD2023PD_CARVER_H
-#define ICCAD2023PD_CARVER_H
+#ifndef ICCAD2023PD_CARVER_T_H
+#define ICCAD2023PD_CARVER_T_H
 
 #include "polygon/polygon_forest_t.h"
 
-class carver {
+class carver_t {
     polygon_forest_t polygon_forest;
     std::vector<rect_t> rects;
     int push_test(int index, rect_t rect);
@@ -15,7 +15,8 @@ class carver {
 public:
     polygon_forest_t carving(polygon_forest_t& polygon_forest);
     polygon_forest_t pushing(polygon_forest_t& polygon_forest);
+    polygon_forest_t best_pf(polygon_forest_t& polygon_forest0, polygon_forest_t& polygon_forest1);
 };
 
 
-#endif //ICCAD2023PD_CARVER_H
+#endif //ICCAD2023PD_CARVER_T_H
