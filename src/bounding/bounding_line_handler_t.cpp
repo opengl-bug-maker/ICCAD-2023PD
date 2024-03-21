ce02ab87d854aed298e730008d807aaeff332467
LenfIcefrost <david77988@gmail.com>
2024-03-21 19:59:27 +0800

    feat: bd line handler add bd line feat

--- src/bounding/bounding_line_handler_t.cpp
+++ src/bounding/bounding_line_handler_t.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "bounding_line_handler_t.h"
+#include "bounding_line_t.h"
 
 bounding_line_handler_t::bounding_line_handler_t(const vec2d_t &size) {
     this->bounding_lines.push_back(bounding_line_t({
@@ -10,7 +11,7 @@ bounding_line_handler_t::bounding_line_handler_t(const vec2d_t &size) {
         vec2d_t(0, size.get_y()),
         size,
         vec2d_t(size.get_x(), 0)
-    },true));
+    },false));
 }
 
 const std::vector<bounding_line_t>& bounding_line_handler_t::get_bounding_lines() const {
@@ -18,9 +19,18 @@ const std::vector<bounding_line_t>& bounding_line_handler_t::get_bounding_lines(
 }
 
 void bounding_line_handler_t::add_bounding_line(const bounding_line_t &bounding_line) {
-    for(auto bl = this->bounding_lines.end(); bl != this->bounding_lines.begin(); bl--){
-        if(bl->touch(bounding_line)){
-//            bounding_lin
+    auto it = this->bounding_lines.begin();
+    while(it != this->bounding_lines.end()) {
+        if(it->get_clockwise()) {
+            it++;
+            continue;
         }
+        if(!it->collision(bounding_line)) {
+            it++;
+            continue;
+        }
+        auto result = merge(*it, bounding_line);
+        *it = result.difference_pos_line[0];
+        it++;
     }
 }
