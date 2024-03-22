81577bc77f31832aefa2499e1edc5ad9f52eb3e6
LenfIcefrost <david77988@gmail.com>
2024-03-22 22:09:15 +0800

    feat: visualizer plot polygon detail

--- src/plugin/visualizer_t.cpp
+++ src/plugin/visualizer_t.cpp
@@ -76,6 +76,58 @@ void visualizer_t::gnup_th(const std::vector<std::pair<std::vector<vec2d_t>,std:
     //reset window name
 }
 
+void visualizer_t::polygon_detail_th(const std::vector<vec2d_t>& points, std::string window_name) {
+    int width = visualizer_t::max_x;
+    int height = visualizer_t::max_y;
+    double canvas_size = 600.0f * double(max_x) / double(max_y);
+    FILE* pipe = popen(true ? "gnuplot -persist" : "gnuplot", "w");
+    fputs(("set term qt size " + std::to_string(canvas_size) + ",600\n").c_str(), pipe);
+    fputs(("set term qt title \"" + window_name + "\"\n").c_str(), pipe);
+    fputs("set grid\n", pipe);
+    fputs("set mxtics 5\n", pipe);
+    fputs("set mytics 5\n", pipe);
+    fputs(("set xrange [0:" + std::to_string(width) + "]\n").c_str(), pipe);
+    fputs(("set yrange [0:" + std::to_string(height) + "]\n").c_str(), pipe);
+
+    std::string str = "";
+    std::stringstream stream;
+    str = "set object 1 polygon from ";
+    for(int m = 0; m < points.size(); m++){
+        str += std::to_string(points[m].get_x());
+        str += ",";
+        str += std::to_string(points[m].get_y());
+        str += " to ";
+    }
+    str += std::to_string(points[0].get_x());
+    str += ",";
+    str += std::to_string(points[0].get_y());
+    str += " \n";
+    fputs(str.c_str(), pipe);
+
+    const uint32_t r = (rand() % 0xBF);
+    const uint32_t g = (rand() % 0xBF);
+    const uint32_t b = (rand() % 0xBF);
+    const uint32_t rgb = r << 16 | g << 8 | b;
+    stream.str("");
+    stream << "0x"
+            << std::setfill ('0') << std::setw(6)
+            << std::hex << rgb;
+
+    str = "set object 1 fc rgb " + stream.str() + " "
+            "fillstyle transparent solid 0.2 "
+            "border lc rgb " + stream.str() + " lw 0.3" " \n";
+    fputs(str.c_str(), pipe);
+    stream.clear();
+    for(int i = 0; i < points.size(); ++i) {
+        fputs(("set label \"" + std::to_string(i) + "\" at " +
+            std::to_string(points[i].get_x()) + "," +
+            std::to_string(points[i].get_y()) + "\n").c_str(), pipe);
+    }
+        
+    fputs("plot 0\n", pipe);
+    if(pipe != nullptr) { pclose(pipe); }
+}
+
 void visualizer_t::join(const std::vector<std::pair<std::vector<vec2d_t>,std::string>>& polys_pts, std::string window_name){
     std::thread th(visualizer_t::gnup_th, polys_pts, window_name);
     th.detach();
@@ -189,4 +241,12 @@ void visualizer_t::draw_bounding_line_connection(std::vector<std::pair<std::vect
     }
     visualizer_t::draw_bounding_line(bounding_lines);
     return ;
+}
+
+void visualizer_t::polygon_detail(const std::vector<vec2d_t>& points, std::string window_name) {
+    visualizer_t::max_x = chip_t::get_width();
+    visualizer_t::max_y = chip_t::get_height();
+    std::thread th(visualizer_t::polygon_detail_th, points, window_name);
+    th.detach();
+    return;
 }
--- src/plugin/visualizer_t.h
+++ src/plugin/visualizer_t.h
@@ -17,12 +17,14 @@ public:
     static void show_fp_rect_no_border(const std::vector<std::pair<rect_t, std::string>> &, const std::string&);
     static void set_window_name(const std::string&);
     static void draw_bounding_line_connection(std::vector<std::pair<std::vector<vec2d_t>,std::string>> bounding_lines);
+    static void polygon_detail(const std::vector<vec2d_t>& points, std::string window_name);
 private:
     static int max_x;
     static int max_y;
     static std::string name;
     static void join(const std::vector<std::pair<std::vector<vec2d_t>,std::string>>& polys_pts, std::string window_name);
     static void gnup_th(const std::vector<std::pair<std::vector<vec2d_t>,std::string>> polys_pts, std::string window_name);
+    static void polygon_detail_th(const std::vector<vec2d_t>& points, std::string window_name);
 };
 
 
